907b927fff67b21d342df5d2a51bd86fd9d9c33b
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2023-09-30 22:26:19 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const { default: mongoose } = require('mongoose')
 
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors({ credentials: true, origin: 'https://blog-client-0mkx.onrender.com' }))
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
