7abfd6404a554a5a3b7b421fab94a93456f6d0dd
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2023-09-30 17:43:16 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
-mongoose.connect('mongodb+srv://blog:1234@cluster.xghc8d4.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('MONGODB_URI')
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body
